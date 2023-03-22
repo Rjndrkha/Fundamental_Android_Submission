b20cd1cb12b8ca70e37d37f48101e443ff935d28
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2023-03-22 13:19:44 +0700

    Final Submission Done

--- app/src/main/java/com/rajendra/dicodingsubmm/adapter/OnItemClickCallback.kt
+++
@@ -1,7 +0,0 @@
-package com.rajendra.dicodingsubmm.adapter
-
-import com.rajendra.dicodingsubmm.datasource.UserResponse
-
-interface OnItemClickCallback {
-    fun onItemClicked(user: UserResponse)
-}
